87105c3205884fff9def7e588500251196adfd6b
a5hil <ashil555anto@gmail.com>
2026-07-15 22:32:10 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AddPet from './Components/AddPet'
+import ViewPets from './Components/ViewPets'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<AddPet />}></Route>
+          <Route path='/view-pets' element={<ViewPets />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
